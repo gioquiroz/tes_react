15efff5df165dcb95307fefe39cd6b2ade13d5d5
Manuel Valencia <valenciamanuel803@gmail.com>
2025-11-18 12:11:22 -0500

    iamgenes
    Se agregaron iamgenes duplicada en tienda para tener una mejor imagen del resultado final

--- nuevo-proyecto-tes/src/data/ShopData.js
+++ nuevo-proyecto-tes/src/data/ShopData.js
@@ -1,4 +1,7 @@
 // --- Datos para simular la API de la tienda ---
+import RICOH from '../assets/RICOH.jpg'
+import foto from '../assets/fotocopiadora.webp'
+import escaner from '../assets/escaner.jpg'
 
 const shopData = {
     "Impresoras": {
@@ -9,10 +12,10 @@ const shopData = {
             "Marcas": ['Ricoh', 'HP', 'Epson', 'Brother', 'Canon'],
         },
         products: [
-            { id: 1, name: "Ricoh Sp 840DN", price: 1000000, brand: "Ricoh", type: "Laser", color: "Blanco y negro", image_url: "https://via.placeholder.com/150/0000FF/808080?text=I1" },
-            { id: 2, name: "Epson EcoTank L3250", price: 850000, brand: "Epson", type: "Inyección", color: "Color", image_url: "https://via.placeholder.com/150/FF0000/FFFFFF?text=I2" },
-            { id: 3, name: "HP LaserJet Pro M404n", price: 1200000, brand: "HP", type: "Laser", color: "Blanco y negro", image_url: "https://via.placeholder.com/150/00FF00/808080?text=I3" },
-            { id: 4, name: "Brother MFC-L3770CDW", price: 1800000, brand: "Brother", type: "Laser", color: "Color", image_url: "https://via.placeholder.com/150/00FFFF/000000?text=I4" },
+            { id: 1, name: "Ricoh Sp 840DN", price: 1000000, brand: "Ricoh", type: "Laser", color: "Blanco y negro", image_url: RICOH },
+            { id: 2, name: "Epson EcoTank L3250", price: 850000, brand: "Epson", type: "Inyección", color: "Color", image_url: RICOH },
+            { id: 3, name: "HP LaserJet Pro M404n", price: 1200000, brand: "HP", type: "Laser", color: "Blanco y negro", image_url: RICOH },
+            { id: 4, name: "Brother MFC-L3770CDW", price: 1800000, brand: "Brother", type: "Laser", color: "Color", image_url: RICOH },
         ]
     },
     "Fotocopiadoras": {
@@ -23,8 +26,8 @@ const shopData = {
             "Marcas": ['Ricoh', 'Konica Minolta', 'Xerox'],
         },
         products: [
-            { id: 10, name: "Ricoh IM 4000", price: 5000000, brand: "Ricoh", speed: "31-45", functionality: "Multifuncional", image_url: "https://via.placeholder.com/150/000000/FFFFFF?text=F1" },
-            { id: 11, name: "Konica C308", price: 8000000, brand: "Konica Minolta", speed: "20-30", functionality: "Multifuncional", image_url: "https://via.placeholder.com/150/999999/FFFFFF?text=F2" },
+            { id: 10, name: "Ricoh IM 4000", price: 5000000, brand: "Ricoh", speed: "31-45", functionality: "Multifuncional", image_url: foto },
+            { id: 11, name: "Konica C308", price: 8000000, brand: "Konica Minolta", speed: "20-30", functionality: "Multifuncional", image_url: foto },
         ]
     },
     "Escaneres": {
@@ -35,8 +38,8 @@ const shopData = {
             "Marcas": ['Epson', 'Canon', 'HP'],
         },
         products: [
-            { id: 20, name: "Epson DS-780N", price: 2500000, brand: "Epson", connectivity: "Red", adf: "Sí", image_url: "https://via.placeholder.com/150/008080/FFFFFF?text=E1" },
-            { id: 21, name: "Canon LIDE 400", price: 500000, brand: "Canon", connectivity: "USB", adf: "No", image_url: "https://via.placeholder.com/150/800080/FFFFFF?text=E2" },
+            { id: 20, name: "Epson DS-780N", price: 2500000, brand: "Epson", connectivity: "Red", adf: "Sí", image_url: escaner },
+            { id: 21, name: "Canon LIDE 400", price: 500000, brand: "Canon", connectivity: "USB", adf: "No", image_url: escaner },
         ]
     }
 };
